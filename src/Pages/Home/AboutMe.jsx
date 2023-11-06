41f92569cac5953d0460ce98f217d5042502a82b
samiNedjai <sami.nedjai@outlook.fr>
2023-11-06 18:19:50 +0100

    Commit initial

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/photo-sami2.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Ancien officier de carrière dans l’armée algérienne, 
+          je souhaite prendre un nouveau tournant dans ma carrière professionnelle en m’orientant en informatique. 
+          Persévérant et très engagé, c’est cet esprit qui m’a toujours animé jusqu’à l‘aboutissement de mes projets.
+           J’ai par ailleurs commencé en autodidacte l’apprentissage de langages informatiques pour acquérir les bases et suivi des formations à l’école Simplon.
+          </p>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
+            recusandae?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,27 @@
+import CVPdf from '../../assete/CV.pdf';
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm SAMI</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <a href={CVPdf} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+          Get In Touch
+        </a>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/photo-sami.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
