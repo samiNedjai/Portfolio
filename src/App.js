9fca1781c8ff1422ebfab7cc32034c98d9f54f56
samiNedjai <sami.nedjai@outlook.fr>
2023-11-06 18:24:08 +0100

    deploy app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/Portfolio" element={<Home />}></Route>
             <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
